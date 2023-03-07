c2825f76c9cb5711cdc99dc7383daf2e9e4121be
Zeeroon-e <chia.kmohamad@hotmail.com>
2023-03-07 09:44:20 +0100

    dockerinstalled

--- src/components/SignupBox.tsx
+++ src/components/SignupBox.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
-
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
+import './signupbox.scss'
 function SignupBox() {
+    const navigate = useNavigate();
+    const [modal, setModal] = useState(false);
+
+    const toggleModal = () => {
+        setModal(!modal)
+    }
+
+    const redirectLogin = () => {
+        navigate('/');
+    }
+
   return (
-    <div>
-      
+    <div className='login-container'>
+        <h2>Together</h2>
+        <div className='input-field'>
+            <label>E-mail</label>
+            <input type="text"  className='email-input'/>    
+        </div>
+        <div className='input-field'>
+            <label>Password</label>
+            <input type="text"  className='password-input'/>    
+        </div>
+        <button className='sign-in-btn' onClick={toggleModal}>Sign up</button>
+        <div className='signup-section'>
+          <p>Have an account?</p>
+          <button className='sign-in-btn' >Sign in</button>
+        </div>
+        { modal && (
+
+            <div className='modal'>
+                <div className='overlay'>
+                    <p>You have succesfully signed up!</p>
+                    <button onClick={redirectLogin}>Close</button>
+                </div>
+            </div>
+        )}
+        
+        
     </div>
   )
 }
--- src/views/SignUp.tsx
+++ src/views/SignUp.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { useState } from 'react';
+import SignupBox from '../components/SignupBox';
+
+
 
 function SignUp() {
+    const [openPop, setOpenPop] = useState(false);
+
+    const signUpPressed = () =>{
+        {setOpenPop(true)}
+    }
   return (
     <div>
-      signup
+      <SignupBox />
       
     </div>
   )
