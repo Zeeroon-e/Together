d9c767913bb414ea15b1b801a366ddcaf16d9a24
Zeeroon-e <chia.kmohamad@hotmail.com>
2023-03-08 08:13:59 +0100

    updated users routeer

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -11,51 +11,49 @@ router.get('/', async (req, res) => {
 
 
 router.post('/login', async (req, res) => {
-     const credentials = req.body;
-     const check = await User.find(credentials);
-    
-        console.log(check)
     
     try {
-        
+        const credentials = req.body;
+        const user = await User.findOne(credentials); 
 
-       if (check) {
-        console.log(check);
-        
-        res.status(200).json(check);
-        
-       }
-       if (!check) {
-        res.status(200).json("user doesnt exist");
-       }
-        
-        
+        if (user) {
+            res.status(200).json(user)
+            
+        } else { 
+          res.status(400).json("check if email and password are correct. If you dont have an account make sure to sign up")  
+        }
 
-        // console.log(credentials.email)
-        // users.forEach(user => {
-        
-        //     if (user.email == credentials.email && user.password == credentials.password) {
-        //         console.log("Found: ", user)
-        //         res.status(200).json(user)
-        //         return;
-                
-        //     } 
-        // });
-        // res.status(400).json("bad")
     } catch (err) {
         console.error(err);
     }
 });
 
-router.post('/signup',  (req, res) => {
-    console.log(req.body);
-    const user = new User(req.body);
+router.post('/signup', async (req, res) => {
+    const credentials = req.body;
+    
+
+    const users = await User.find();
+    
     try {
-       user.save(); 
-       res.status(201).json({user: user});
+
+        if (users) {
+
+            const checkUser = users.find((user) => user.email === credentials.email);
+            
+            if (!checkUser) {
+                const user = new User(req.body);
+                user.save();
+                res.status(201).json({User: user});
+            } else {
+
+                console.log(checkUser.email + " is already in use");
+                res.status(400).json("user already exists");
+            }
+                
+        }
 
     } catch (err) {
-        log.error(err);
+        console.error(err);
     }
 });
 
